b6fa931bcd03193d34a4745e183a109c6ee9b65e
barbaratomaz2005-oss <barbaratomaz2005@gmail.com>
2026-07-20 22:07:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,28 @@
+// =====================================
+// CONFIGURAÇÃO DA PLANILHA GOOGLE SHEETS
+// =====================================
 
+const SHEET_ID = "15elqBXpHgUMZu9GCxOuQzeHfYvb7Zsv0QVWl0FfgRwc";
+
+// =====================================
+// ABAS DA PLANILHA
+// =====================================
+
+const GIDS = {
+  config: "295667101",
+  avancoFisicoSemanal: "873446954",
+  avancoFisicoAcumulado: "1809824478",
+  atividadesSemanaisFotos: "376787791",
+  atividadesSemanaisPontos: "1332500257",
+  fluxoFinanceiroSemanal: "1676535506",
+  fluxoFinanceiroTrimestral: "173394064",
+  curvaS: "334216643",
+  contratacoes: "1718836335",
+  concessionarias: "723176947",
+  canteiroFuncionarios: "788302874",
+  rocontec: "1459457304"
+};
+
+// Disponibiliza para os outros arquivos
+window.SHEET_ID = SHEET_ID;
+window.GIDS = GIDS;
